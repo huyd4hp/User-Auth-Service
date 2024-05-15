fb536e3771307c2e43593a5930a002e0df4b00fd
Nhật Huy <nhathuyd4hp@gmail.com>
2024-05-15 17:46:16 +0700

    update service

--- src/service/auth.services.js
+++ src/service/auth.services.js
@@ -2,39 +2,57 @@ const UserService = require("../service/user.services");
 const JWTFactory = require("../helper/jwt_service");
 const blacklistModel = require("../model/blacklist.model");
 const bcrypt = require("bcrypt");
+const { responseUser } = require("../helper/getData");
 const clientRedis = require("../databases/redis");
 class AuthService {
   static SignUp = async (data) => {
+    // 1. Xác thực thông tin đăng kí
     const holderUser = await UserService.FindUserByEmail(data.email);
     if (holderUser) {
       return null;
     }
+    // 2. Tạo User mới trả về controller
     const newUser = await UserService.AddUser(data);
     return newUser;
   };
   static LogIn = async (email, password) => {
+    // 1. Xác thực thông tin đăng nhập
+    /// 1. Kiểm tra email
     const holderUser = await UserService.FindUserByEmail(email);
     if (!holderUser) {
       return null;
     }
+    /// 2. Kiểm tra password
     const checkPassword = bcrypt.compareSync(password, holderUser.password);
     if (!checkPassword) {
       return null;
     }
-    return holderUser;
+    // 2. Tạo AccessToken và RefreshToken
+    const payload = responseUser(holderUser);
+    const AT = JWTFactory.generalAccessToken(payload);
+    const RT = JWTFactory.generalRefreshToken(payload);
+    // 3. Thêm RefreshToken vào Redis (Tạo Session cho User)
+    await clientRedis.set(holderUser._id.toString(), RT);
+    // 4. Trả metadata (AT,RT,User) về controller
+    const metadata = {
+      user: payload,
+      access_token: AT,
+      refresh_token: RT,
+    };
+    return metadata;
   };
   static LogOut = async (token) => {
-    const userId = await JWTFactory.verifyRefreshToken(token);
-    // Invalid token
+    // 1. Xác thực RefreshToken
+    const userId = JWTFactory.verifyRefreshToken(token);
     if (!userId) {
       return false;
     }
-    // Check session
-    sessionToken = clientRedis.get(userId.toString());
+    // 2. Check session của userId trong Redis
+    const sessionToken = await clientRedis.get(userId.toString());
     if (token !== sessionToken) {
       return false;
     }
-    // Delete session
+    // 3. Delete session trong Redis (Thêm vào blacklistModel)
     await blacklistModel.create({
       user: userId,
       refreshToken: token,
@@ -43,23 +61,23 @@ class AuthService {
     return true;
   };
   static RefreshToken = async (token) => {
-    // Check token
-    const userId = await JWTFactory.verifyRefreshToken(token);
+    // 1. Xác thực RefreshToken
+    const userId = JWTFactory.verifyRefreshToken(token);
     if (!userId) {
       return null;
     }
-    // Check session
+    // 2. Check session của userId
     const usingToken = await clientRedis.get(userId.toString());
     if (usingToken !== token) {
       return null;
     }
-    // Renew token
+    // 3. Renew token cho userId
     const user = await UserService.FindUserById(userId);
     if (!user) {
       return null;
     }
     const payload = responseUser(user);
-    const newToken = await JWTFactory.generalAccessToken(payload);
+    const newToken = JWTFactory.generalAccessToken(payload);
     return newToken;
   };
 }
--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,6 +1,12 @@
 const userModel = require("../model/user.model");
 const bcrypt = require("bcrypt");
+const clientRedis = require("../databases/redis");
 class UserService {
+  static GetUsers = async (page = 1) => {
+    const skip = (page - 1) * 50;
+    const users = await userModel.find().skip(skip).limit(50);
+    return users;
+  };
   static FindUserById = async (id) => {
     try {
       const user = await userModel.findById(id).lean();
@@ -12,6 +18,9 @@ class UserService {
   };
   static FindUserByEmail = async (email) => {
     const user = await userModel.findOne({ email: email }).lean();
+    if (!user) {
+      return null;
+    }
     return user;
   };
   static AddUser = async (data) => {
@@ -21,7 +30,34 @@ class UserService {
     const newUser = await userModel.create(data);
     return newUser;
   };
-  static UpdateUser = () => {};
-  static DeleteUser = () => {};
+  static DeleteUserById = async (id) => {
+    // 1. Xoá document trong MongoDB
+    const action = await userModel.deleteOne({ _id: id }).lean();
+    // 2. Xoá session nếu có trong Redis
+    await clientRedis.del(id);
+    return action.deletedCount > 0;
+  };
+  static UpdateProfile = async (userid, newProfile) => {
+    const newUser = await userModel.findByIdAndUpdate(userid, newProfile, {
+      new: true,
+    });
+  };
+  static UpdatePassword = async (userid, newPassword) => {
+    const salt = await bcrypt.genSalt(1024);
+    const hashPassword = await bcrypt.hash(newPassword, salt);
+    const user = await userModel.findByIdAndUpdate(
+      userid,
+      {
+        password: hashPassword,
+      },
+      {
+        new: true,
+      }
+    );
+    if (!user) {
+      return null;
+    }
+    return user;
+  };
 }
 module.exports = UserService;
