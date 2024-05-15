c78057c0b127340672f4bffcd75284a33ef6cbcc
Nhật Huy <nhathuyd4hp@gmail.com>
2024-05-15 17:44:42 +0700

    config mongo

--- src/databases/mongo/index.js
+++ src/databases/mongo/index.js
@@ -1,29 +1,33 @@
 const mongoose = require("mongoose");
-const mongo = require("../../config/").mongo
-const connectString = `mongodb://${mongo.username}:${mongo.password}@${mongo.host}:${mongo.port}`
-class Database{
-    constructor(){
-        this.connect()
+const mongo = require("../../config/").mongo;
+const debug = require("../../config").app.debug;
+const connectString = `mongodb://${mongo.username}:${mongo.password}@${mongo.host}:${mongo.port}`;
+class Database {
+  constructor() {
+    this.connect();
+  }
+  connect() {
+    if (debug) {
+      mongoose.set("debug", true);
+      mongoose.set("debug", { color: true });
     }
-    connect(){
-        mongoose.connect(connectString,{
-            dbName:mongo.database,
-        })
-        .then( _ => {
-            console.log("INFO:   Connected to MongoDB");
-        }
-        ).catch(error => console.log(`Error:  ${error}!`))
+    mongoose
+      .connect(connectString, {
+        dbName: mongo.database,
+      })
+      .then((_) => {
+        console.log("INFO:   Connected to MongoDB");
+      })
+      .catch((error) => console.log(`Error:  ${error}!`));
+  }
+  //
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
     }
-    // 
-    static getInstance(){
-        if(!Database.instance)
-        {
-            Database.instance = new Database()
-        }
-        return Database.instance
-    }
+    return Database.instance;
+  }
 }
 
-const instance = Database.getInstance()
-module.exports = instance
-
+const instance = Database.getInstance();
+module.exports = instance;
