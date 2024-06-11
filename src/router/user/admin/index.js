7efcf8bd2ed22a2d17a5c77b4fc2e35a856e9c23
Nhật Huy <nhathuyd4hp@gmail.com>
2024-06-11 16:31:10 +0700

    Add Block/Unblock endpoint

--- src/router/user/admin/index.js
+++ src/router/user/admin/index.js
@@ -5,6 +5,10 @@ const UserController = require("../../../controller/user.controller");
 router.get("/users", handler(UserController.GetUsers));
 router.get("/user/:id", handler(UserController.GetUser));
 router.delete("/user/:id", handler(UserController.DeleteUser));
+
+router.put("/block/:id", handler(UserController.BlockUser));
+router.put("/unblock/:id", handler(UserController.UnblockUser));
+
 router.post("/user", handler(UserController.AddUser));
 
 module.exports = router;
