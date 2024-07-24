3b86c9708df98d77ed20c20db23bf9f7960baca5
Nhật Huy <nhathuyd4hp@gmail.com>
2024-07-25 04:47:47 +0700

    update for gateway

--- setup_mongo.sh
+++ setup_mongo.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 # Load các biến môi trường từ file .env
 MONGO_HOST_PRIMARY=$(grep MONGO_HOST_PRIMARY .env | cut -d '=' -f 2)
-MONGO_HOST_SLAVE_1=$(grep MONGO_HOST_SLAVE_1 .env | cut -d '=' -f 2)
-MONGO_HOST_SLAVE_2=$(grep MONGO_HOST_SLAVE_2 .env | cut -d '=' -f 2)
+MONGO_HOST_SECONDARY_1=$(grep MONGO_HOST_SECONDARY_1 .env | cut -d '=' -f 2)
+MONGO_HOST_SECONDARY_2=$(grep MONGO_HOST_SECONDARY_2 .env | cut -d '=' -f 2)
 MONGO_USERNAME=$(grep MONGO_USERNAME .env | cut -d '=' -f 2)
 MONGO_PASSWORD=$(grep MONGO_PASSWORD .env | cut -d '=' -f 2)
-# Cấu hình Replicaset
-docker exec -i AuthDB-Master mongosh << EOF
-rs.initiate({
-  _id: 'rs0',
-  members: [
-    { _id: 0, host: '${MONGO_HOST_PRIMARY}:27017' },
-    { _id: 1, host: '${MONGO_HOST_SLAVE_1}:27017' },
-    { _id: 2, host: '${MONGO_HOST_SLAVE_2}:27017' }
-  ]
-})
-EOF 
-Setup Username-Password
-docker exec -i AuthDB-Master mongosh << EOF
-use admin;
-db..createUser({
-    user: '${MONGO_USERNAME}',
-    pwd: '${MONGO_PASSWORD}',
-    roles: [{ role: 'root', db: 'admin' }]
-});
-EOF
 
+# Setup Replicaset
+status=$(docker exec -i mongo.pauth mongosh --eval "rs.status()" --quiet 2>/dev/null)
+if [[ -z "$status" ]]; then
+  docker exec -i mongo.pauth mongosh --eval "rs.initiate({
+    _id: 'rs0',
+    members: [
+      { _id: 0, host: '${MONGO_HOST_PRIMARY}:27017', priority: 2 },
+      { _id: 1, host: '${MONGO_HOST_SECONDARY_1}:27017', priority: 1 },
+      { _id: 2, host: '${MONGO_HOST_SECONDARY_2}:27017', priority: 1 }
+    ]
+  })"
+  echo '************************************'
+  echo '*    Replica set setup complete    *'
+  echo '************************************'
 
-
-
-
-
+else
+  echo '
+-------------------------------
+Replica set already initialized 
+-------------------------------'
+fi
+# Setup Authenticated
+auth=$(docker exec -i mongo.pauth mongosh --eval "$(echo "db.auth('$MONGO_USERNAME', '$MONGO_PASSWORD')" | tr -d ' ')" --quiet 2>/dev/null)
+if [[ -z "$auth" ]]; then
+  authCommand="$(echo "db.createUser({user: '$MONGO_USERNAME', pwd: '$MONGO_PASSWORD', roles: [{role: 'root', db: 'admin'}]})" | tr -d ' ')" 
+  docker exec -i mongo.pauth mongosh --eval "use admin; $authCommand"
+fi
+echo '
+-------------------------------
+MONGO_ROOT_USERNAME:  root
+MONGO_ROOT_PASSWORD:  rootMongo
+-------------------------------'
--- src/databases/mongo/index.js
+++ src/databases/mongo/index.js
@@ -1,43 +1,43 @@
-  const mongoose = require("mongoose");
-  const colors = require("colors");
-  const {
-    MONGO_USERNAME,
-    MONGO_PASSWORD,
-    MONGO_HOST_PRIMARY,
-    MONGO_HOST_SLAVE_1,
-    MONGO_HOST_SLAVE_2,
-    MONGO_PORT,
-    MONGO_DATABASE,
-  } = require("../../config");
-  class Database {
-    constructor() {
-      this.connectString = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST_PRIMARY}:${MONGO_PORT},${MONGO_HOST_SLAVE_1}:${MONGO_PORT},${MONGO_HOST_SLAVE_2}:${MONGO_PORT}/?replicaSet=rs0`;
-      this.connect();
-    }
-    connect() {
-      mongoose
-        .connect(this.connectString, {
-          dbName: MONGO_DATABASE,
-        })
-        .then((_) => {
-          console.log(colors.green("INFO:    "), "Connected to MongoDB");
-        })
-        .catch((error) => {
-          console.log(colors.red("ERROR:    "), `${error}`);
-          setTimeout(() => {
-            console.log(colors.yellow("INFO:    "), "Reconnect to MongoDB");
-            this.connect();
-          }, 5); //
-        });
-    }
-    //
-    static getInstance() {
-      if (!Database.instance) {
-        Database.instance = new Database();
-      }
-      return Database.instance;
-    }
+const mongoose = require("mongoose");
+const colors = require("colors");
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOST_PRIMARY,
+  MONGO_HOST_SECONDARY_1,
+  MONGO_HOST_SECONDARY_2,
+  MONGO_PORT,
+  MONGO_DATABASE,
+} = require("../../config");
+class Database {
+  constructor() {
+    this.connectString = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST_PRIMARY}:${MONGO_PORT},${MONGO_HOST_SECONDARY_1}:${MONGO_PORT},${MONGO_HOST_SECONDARY_2}:${MONGO_PORT}/test?replicaSet=rs0`;
+    this.connect();
   }
+  connect() {
+    mongoose
+      .connect(this.connectString, {
+        dbName: MONGO_DATABASE,
+      })
+      .then((_) => {
+        console.log(colors.green("INFO:    "), "Connected to MongoDB");
+      })
+      .catch((error) => {
+        console.log(colors.red("ERROR:    "), `${error}`);
+        setTimeout(() => {
+          console.log(colors.yellow("INFO:    "), "Reconnect to MongoDB");
+          this.connect();
+        }, 5); //
+      });
+  }
+  //
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+}
 
-  const instance = Database.getInstance();
-  module.exports = instance;
+const instance = Database.getInstance();
+module.exports = instance;
